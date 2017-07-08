f2336ed05745d5a663e1dc4e8bf944ee3c2a750c
svetlyobg <swk_12@abv.bg>
2017-07-08 20:14:44 +0300

    Променливи

---
+++ 7.Variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+		int age;
+		age = 23;
+		/* same as int age = 23; */
+
+		printf("Svetlozar is %d years old.\n", age);
+
+		int currentYear, yearOfBirth, personAge;
+		currentYear = 2017;
+		yearOfBirth = 1993;
+		personAge = 2017 - 1993;
+		printf("Svetlozar is %d years old.\n", personAge);
+		return 0;
+}
