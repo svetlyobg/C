897a851c9b46610fa08d24974429760ef49f5a8f
svetlyobg <swk_12@abv.bg>
2017-07-08 20:13:53 +0300

    Изход на конзолата

---
+++ 4.PrintTextOnTheScreen.c
@@ -0,0 +1,11 @@
+/*https://www.youtube.com/watch?v=oSpmApiUsHw&index=4&list=PL6gx4Cwl9DGAKIXv8Yr6nhGJ9Vlcjyymq*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+		printf("Svetlyo is awsome\n");
+		printf("Svetlyo is cool\n");
+	return 0;
+}
