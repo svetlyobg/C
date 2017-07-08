9318d6f00a744c3419a5d113994d108d01bd3f3c
svetlyobg <swk_12@abv.bg>
2017-07-08 20:14:34 +0300

    Форматиране на стринг

---
+++ 6.ConversionCharacters.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+		printf("%s is the best %s ever!\n","Svetlozar","programmer");
+		printf("I ate %d sandwiches last night!\n", 6);
+		printf("My body temperature is %f.\n", 36.52);
+		printf("Yesterday it was %.2f.\n", 36.04);
+		
+		return 0;
+}
